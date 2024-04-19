8dea1e4b9d63aa51ced63fcbfda351a25c1e556d
Peter So <peterjso23@gmail.com>
2024-04-19 19:12:04 -0400

    added all client functions

---
+++ src/Spotify/client.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const axiosWithCredentials = axios.create({
+  withCredentials: true,
+});
+const NODE_API = process.env.BASE_API;
+
+export const getTodaysTopHits = async () => {
+  const response = await axiosWithCredentials.get(
+      `${NODE_API}/api/spotify/todays-top-hits`
+  );
+
+  return response.data;
+}
+
+export const searchTracks = async (query: String) => {
+  const response = await axiosWithCredentials.get(
+      `${NODE_API}/api/spotify/search/tracks/${query}`
+  );
+
+  return response.data;
+}
+
+export const getTrack = async (trackId: any) => {
+  const response = await axiosWithCredentials.get(
+      `${NODE_API}/api/spotify/tracks/${trackId}`
+  );
+
+  return response.data;
+}
---
+++ src/Tune/client.ts
@@ -0,0 +1,113 @@
+import axios from "axios";
+
+const axiosWithCredentials = axios.create({
+  withCredentials: true,
+});
+const NODE_API = process.env.BASE_API;
+
+// Tracks
+const TRACKS_API = `${NODE_API}/api/tracks`;
+
+export const createTrack = async (track: any) => {
+  const response = await axiosWithCredentials.post(
+      TRACKS_API,
+      track
+  );
+
+  return response.data;
+};
+
+export const deleteTrack = async (trackId: any) => {
+  const response = await axiosWithCredentials.delete(
+      `TRACKS_API/${trackId}`,
+  );
+
+  return response.data;
+};
+
+export const updateTrack = async (trackId: any) => {
+  const response = await axiosWithCredentials.put(
+      `TRACKS_API/${trackId}`,
+  );
+
+  return response.data;
+};
+
+export const getAllTracks = async () => {
+  const response = await axiosWithCredentials.get(
+      TRACKS_API,
+  );
+
+  return response.data;
+};
+
+export const getTrackById = async (trackId: any) => {
+  const response = await axiosWithCredentials.get(
+      `TRACKS_API/${trackId}`,
+  );
+
+  return response.data;
+};
+
+// Likes
+const LIKES_API = `${NODE_API}/api/likes`;
+
+export const likeTrack = async (track: any) => {
+  const response = await axiosWithCredentials.post(
+      `${LIKES_API}/track`,
+      track
+  );
+
+  return response.data
+};
+
+export const unlikeTrack = async (trackId: any) => {
+  const response = await axiosWithCredentials.delete(
+      `${LIKES_API}/track/${trackId}`
+  );
+
+  return response.data
+};
+
+export const getAllLikedTracks = async () => {
+  const response = await axiosWithCredentials.get(
+      `${LIKES_API}/track`
+  );
+
+  return response.data
+};
+
+// Social
+const SOCIAL_API = `${NODE_API}/api/social`;
+
+export const followUser = async (userId: any) => {
+  const response = await axiosWithCredentials.post(
+      `${SOCIAL_API}/follow/${userId}`
+  );
+
+  return response.data;
+}
+
+export const unfollowUser = async (userId: any) => {
+  const response = await axiosWithCredentials.delete(
+      `${SOCIAL_API}/follow/${userId}`
+  );
+
+  return response.data;
+}
+
+export const getAllFollowing = async (userId: any) => {
+  const response = await axiosWithCredentials.get(
+      `${SOCIAL_API}/following/${userId}`
+  );
+
+  return response.data;
+}
+
+export const getAllFollowers = async (userId: any) => {
+  const response = await axiosWithCredentials.get(
+      `${SOCIAL_API}/followers/${userId}`
+  );
+
+  return response.data;
+}
