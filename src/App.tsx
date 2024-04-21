76b9aca3388d34a3a49a6803fa95d863d9da6932
Peter So <peterjso23@gmail.com>
2024-04-21 12:28:31 -0400

    search finished, need to account for user that is logged in vs not in carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,46 @@
 import React from "react";
-import {Routes, Route, Navigate} from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import Register from "./Users/Register";
 import Login from "./Users/Login";
-import {HashRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Home from "./Home";
 import TrackDetail from "./Tracks/TrackDetail";
 import Profile from "./Users/Profile";
 import EditProfile from "./Users/EditProfile";
 import Navigation from "./Navigation";
-import {Provider} from "react-redux";
-import {PersistGate} from "redux-persist/integration/react";
+import SpotifySearch from "./Spotify/search";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import store from "./Store";
-
 
 function App() {
   return (
-      <>
-        <Provider store={store.store}>
-          <PersistGate persistor={store.persistor} loading={null}>
-            <HashRouter>
-              <Navigation/>
-              <Routes>
-                <Route path="/" element={<Navigate to="/Tune/Home"/>}/>
-                <Route path="/Account/Register" element={<Register/>}/>
-                <Route path="/Account/Login" element={<Login/>}/>
-                <Route path="/Tune/Home" element={<Home/>}/>
-                <Route path="/Tune/Track/:trackId" element={<TrackDetail/>}/>
-                <Route path="/Account/Profile/:uid" element={<Profile/>}/>
-                <Route path="/Account/Profile/Edit/:uid" element={<EditProfile/>}/>
-              </Routes>
-            </HashRouter>
-          </PersistGate>
-        </Provider>
-      </>
+    <>
+      <Provider store={store.store}>
+        <PersistGate persistor={store.persistor} loading={null}>
+          <HashRouter>
+            <Navigation />
+            <Routes>
+              <Route path="/" element={<Navigate to="/Tune/Home" />} />
+              <Route path="/Account/Register" element={<Register />} />
+              <Route path="/Account/Login" element={<Login />} />
+              <Route path="/Tune/Home" element={<Home />} />
+              <Route path="/Tune/Track/:trackId" element={<TrackDetail />} />
+              <Route path="/Tune/Track/Search" element={<SpotifySearch />} />
+              <Route
+                path="/Tune/Track/Search/:query"
+                element={<SpotifySearch />}
+              />
+              <Route path="/Account/Profile/:uid" element={<Profile />} />
+              <Route
+                path="/Account/Profile/Edit/:uid"
+                element={<EditProfile />}
+              />
+            </Routes>
+          </HashRouter>
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
 
